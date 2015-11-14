8b4755fd66aa7c76138b31ecd7d8329f1e7dd0cf
Shunsuke SHIMADA <shsh0310@gmail.com>
2015-11-14 17:54:13 +0900

    placemap for iPad

---
+++ PlaceMap_for_iPad/PlaceMap/MapDisplayViewController.swift
@@ -0,0 +1,120 @@
+//
+//  MapDisplayViewController.swift
+//  PlaceMap
+//
+//  Created by shunsukeshimada on 2015/11/14.
+//  Copyright © 2015年 shunsukeshimada. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapDisplayViewController: UIViewController {
+
+    var selectedIndex : Int = 0
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var placeName: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if(UIDevice.currentDevice().userInterfaceIdiom == .Pad){
+            let svc = self.splitViewController
+            let navItem = self.navigationItem
+            navItem.leftBarButtonItem = svc?.displayModeButtonItem()
+        }
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Selected = \(selectedIndex)")
+        // \は、円記号ではなく、バックスラッシュです。option+¥ で入力できます。
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let places = appDelegate.placeDB.places
+        let prefName = places[selectedIndex]["pref"] as! String
+        let capName = places[selectedIndex]["name"] as! String
+        
+        print("info = \(prefName)\(capName)")
+        
+        self.placeName.text = "\(prefName)[\(capName)]"
+        
+        let coordinate = CLLocationCoordinate2D(
+            latitude: places[selectedIndex]["latitude"] as! CLLocationDegrees,
+            longitude: places[selectedIndex]["longitude"] as! CLLocationDegrees
+        )
+//        self.mapView.centerCoordinate = coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        self.mapView.region = region
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
